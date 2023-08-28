2f7ce2f636e62c47c20df4a7f157b6d64cf86d7a
wh04m1 <49116189+randy1369@users.noreply.github.com>
2023-08-28 12:23:37 +0530

    Update dev-ops.py

--- dev-ops.py
+++ dev-ops.py
@@ -5,7 +5,7 @@ current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 
 def main():
-    
+    st.set_page_config(page_title="DevOps Guide", page_icon=":rocket:")
     st.image("lifecycle.gif", use_column_width=True)
 
     topics = [
@@ -38,6 +38,9 @@ def main():
             st.image("roadmap.png", use_column_width=True)
         else:
             st.markdown(selected_content, unsafe_allow_html=True)
+
+    with open(css_file, "r") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 def read_md_content(filename):
     with open(filename, "r") as md_file:
